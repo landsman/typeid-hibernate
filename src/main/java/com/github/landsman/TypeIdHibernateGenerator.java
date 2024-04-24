8158448e339180f1c0c66e21f51074fd3780dbe2
Michal Landsman <landsman@studioart.cz>
2024-04-24 15:39:56 +0200

    add comments, clean code

--- src/main/java/com/github/landsman/TypeIdHibernateGenerator.java
+++ src/main/java/com/github/landsman/TypeIdHibernateGenerator.java
@@ -7,25 +7,42 @@ import org.hibernate.id.IdentifierGenerator;
 import java.io.Serializable;
 import java.lang.reflect.Field;
 
+/**
+ * Generator class for creating unique identifiers with a prefix.
+ * This class implements the IdentifierGenerator interface to provide
+ * custom identifier generation strategy for entities.
+ */
 public class TypeIdHibernateGenerator implements IdentifierGenerator {
+
+    public TypeIdHibernateGenerator() {
+        super();
+    }
 
     @Override
     public Serializable generate(SharedSessionContractImplementor session, Object obj) throws HibernateException {
         Class<?> entityClass = obj.getClass();
-        String prefix = null;
-
-        for (Field field : entityClass.getDeclaredFields()) {
-            if (field.isAnnotationPresent(TypeIdHibernate.class)) {
-                TypeIdHibernate annotation = field.getAnnotation(TypeIdHibernate.class);
-                prefix = annotation.prefix();
-                break;
-            }
-        }
+        String prefix = findPrefixFromAnnotation(entityClass);
 
         if (prefix == null) {
             throw new HibernateException("No field annotated with @TypeIdHibernate found");
         }
 
-        return String.valueOf(TypeId.generate(prefix));
+        return TypeId.generate(prefix).toString();
+    }
+
+    /**
+     * Find the prefix from the annotation @TypeIdHibernate.
+     * @param entityClass the entity class
+     * @return the prefix
+     */
+    private String findPrefixFromAnnotation(Class<?> entityClass) {
+        for (Field field : entityClass.getDeclaredFields()) {
+            if (field.isAnnotationPresent(TypeIdHibernate.class)) {
+                field.setAccessible(true);
+                TypeIdHibernate annotation = field.getAnnotation(TypeIdHibernate.class);
+                return annotation.prefix();
+            }
+        }
+        return null;
     }
 }
