cdd83e75cdd33d40a64c8ec2583843f179cf4b38
Michal Landsman <landsman@studioart.cz>
2024-04-24 15:29:45 +0200

    publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,15 @@
 plugins {
     id("java")
+    id("maven-publish")
 }
 
 group = "com.github.landsman"
 version = "0.1"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
 
 repositories {
     mavenCentral()
@@ -19,6 +25,60 @@ dependencies {
     testImplementation("org.mockito:mockito-junit-jupiter:5.0.0")
 }
 
-tasks.test {
+tasks.withType<Test> {
     useJUnitPlatform()
+    failFast = true
+    outputs.upToDateWhen { false }
+}
+
+tasks.register<Jar>("sourcesJar") {
+    archiveClassifier.set("sources")
+    from(sourceSets["main"].allSource)
+}
+
+tasks.register<Jar>("javadocJar") {
+    archiveClassifier.set("javadoc")
+    from(tasks.javadoc)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            artifact(tasks["sourcesJar"])
+            artifact(tasks["javadocJar"])
+
+            pom {
+                name.set("typeid-hibernate")
+                description.set("Custom ID generator for Hibernate, Spring Framework inspired by @stripe")
+                url.set("https://github.com/landsman/typeid-hibernate")
+
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://opensource.org/license/mit")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("landsman")
+                        name.set("Michal Landsman")
+                        email.set("landsmichal@gmail.com")
+                    }
+                }
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/landsman/typeid-hibernate")
+            credentials {
+                username = findProperty("gpr.user") as String? ?: System.getenv("GITHUB_ACTOR")
+                password = findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
 }
